94f11d4249f609c13d7796f808cb8f8691edd880
Randy Fuentes <rfuentes@loomissayles.com>
2024-03-06 14:45:10 -0500

    :O/

--- src/pages/_app.page.jsx
+++ src/pages/_app.page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import Head from "next/head";
 import Script from "next/script";
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -7,7 +7,7 @@ import Theme from "../common/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Loading from "../common/loading";
 
 const queryClient = new QueryClient({
@@ -24,26 +24,50 @@ const queryClient = new QueryClient({
 });
 
 export default function App({ Component, pageProps }) {
-  const [officeIsReady, setOfficeIsReady] = useState(false);
-
-  //#region Handle Office On Ready & set Default values
-  const officeOnReadyCallback = useCallback(() => {
-    if (officeIsReady) return;
-    setOfficeIsReady(true);
-  }, [officeIsReady]);
-  useEffect(() => {
-    Office.onReady(officeOnReadyCallback);
-  }, [officeOnReadyCallback]);
-
-  //#endregion
+  const [loadedOffice, setLoadedOffice] = useState(false);
+  const [d, setD] = useState(null);
+  const forceUpdate = React.useCallback(() => setLoadedOffice(true), []);
   return (
     <>
-
+      {/* https://learn.microsoft.com/en-us/answers/questions/1070090/using-office-javascript-api-in-next-js */}
+      {/* Local dev hack */}
+      {process.env.NODE_ENV !== "production" && (
+        <Script
+          id="store_replaceState"
+          dangerouslySetInnerHTML={{
+            __html: "window._replaceState = window.replaceState",
+          }}
+        />
+      )}
+      <Script
+        
+        type="text/javascript"
+        src="https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js"
+        crossOrigin="anonymous"
+        onLoad={() => {
+          Office.initialize = (reason) => {
+            setD(`initialize: ${reason}`);
+            console.log(`initialize: ${reason}`);
+            setLoadedOffice(true);
+            forceUpdate();
+          }; // MUST BE BEFORE Office.onReady
+        }}
+      ></Script>
+      {/* Local dev hack */}
+      {process.env.NODE_ENV !== "production" && loadedOffice && (
+        <Script
+          id="assign_replaceState"
+          dangerouslySetInnerHTML={{
+            __html: "window.history.replaceState = window._replaceState",
+          }}
+        />
+      )}
       <ThemeProvider theme={Theme}>
         <QueryClientProvider client={queryClient}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Loading center isLoading={officeIsReady == false} />
-            {officeIsReady ? <Component {...pageProps} /> : <></>}
+            <Loading center isLoading={loadedOffice == false} />
+            {d}
+            {loadedOffice ? <Component {...pageProps} /> : null}
           </LocalizationProvider>
         </QueryClientProvider>
       </ThemeProvider>
--- src/pages/_document.page.jsx
+++ src/pages/_document.page.jsx
@@ -7,34 +7,12 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
-        {/* https://learn.microsoft.com/en-us/answers/questions/1070090/using-office-javascript-api-in-next-js */}
-        {/* Local dev hack */}
-        {process.env.NODE_ENV !== "production" && (
-          <Script
-            id="store_replaceState"
-            dangerouslySetInnerHTML={{
-              __html: "window._replaceState = window.replaceState",
-            }}
-          />
-        )}
         <Script
           strategy="beforeInteractive"
-          defer={false}
           type="text/javascript"
           src="https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js"
           crossOrigin="anonymous"
         ></Script>
-
-        {/* Local dev hack */}
-        {process.env.NODE_ENV !== "production" && (
-          <Script
-            id="assign_replaceState"
-            dangerouslySetInnerHTML={{
-              __html: "window.history.replaceState = window._replaceState",
-            }}
-          />
-        )}
-
         <Script
           id="office_init"
           dangerouslySetInnerHTML={{
